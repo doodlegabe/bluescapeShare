3a7417f1afb77a37d4aebf396a7f6944fa8d23ee
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-09-20 13:15:55 -0400

    Initial Commit

---
+++ bluescapeShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bluescapeShare
+//
+//  Created by Gabriel Walsh on 9/20/19.
+//  Copyright © 2019 Gabriel Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
