a6b2f4f1f025e942de74f05ba1ea272948a20f87
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-10-07 14:23:34 -1000

    added dummy APIKeys.swift

---
+++ bluescapeShare/_APIKeys.swift
@@ -0,0 +1,20 @@
+//
+//  _APIKeys.swift
+//  bluescapeShare
+//
+//  Created by Gabriel Walsh on 10/7/19.
+//  Copyright © 2019 Gabriel Walsh. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Fill out the values of the struct and rename as APIKeys
+
+//struct APIKeys {
+//    static var bluescapeAuthToken: String = ""
+//      static var bluescapeAPIWorkspaceID: String = ""
+//      static var defaultCanvas: String = ""
+//      static var baseAPIPath: String = "https://api.apps.us.bluescape.com/v1"
+//      static var sharedSuiteName: String =  ""
+//      static var bluescapeWebBaseUrl: String = "https://client.apps.us.bluescape.com"
+//}
